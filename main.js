a9eed9deb1b1c7e10b5fa40f75448b22f0dabf8f
Dmitriy <stupakov.dmitriy1997@gmail.com>
2021-04-08 13:33:20 +0300

    const BIRTHDAY = '18.04.1982'; // использовать заглавные буквы?

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 alert();
+
+let admin, name;
+name="Джон";
+admin=name;
+alert(admin)
+
+let ourPlanetName = "Земля";
+let currentUser;
+
+const BIRTHDAY = '18.04.1982'; // использовать заглавные буквы?
+
+const AGE = someCode(BIRTHDAY); // а здесь?
